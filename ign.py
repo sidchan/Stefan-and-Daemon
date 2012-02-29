6b7eb40acb55aefd46c079d38c9753bbfa2a38d1
unknown <siddharth@.(none)>
2012-03-01 00:59:44 +0530

    Ign ripped

---
+++ ign.py
@@ -0,0 +1,17 @@
+import os
+from urllib2 import urlopen
+from urlparse import urljoin,urlsplit
+from bs4 import BeautifulSoup as bs 
+
+#for i in range(85):
+url="http://m.ign.com/games/reviews-editors-choice/p1"
+soup=bs(urlopen(url).read())
+info=soup.find("ul")
+ab=info.li
+#print str(ab)
+while (ab):
+      name=str(ab.a.findAll("span")[1].h3.contents[0])
+      rating=str(ab.a.span.contents[0])
+      type=str(ab.a.findAll("span")[1].span.contents[0])
+      print name , rating  , type
+      ab=ab.nextSibling.nextSibling
