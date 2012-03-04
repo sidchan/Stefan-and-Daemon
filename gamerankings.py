e1f3fa07eb2d7c7a006ea050b960b3c4cb38f936
unknown <siddharth@.(none)>
2012-03-04 22:11:10 +0530

    game ranking shows a malformed start tag error ,Planning on preproccessing the data or manually parsing :'(

---
+++ gamerankings.py
@@ -0,0 +1,21 @@
+import os
+from urllib2 import urlopen
+from urlparse import urljoin,urlsplit
+from bs4 import BeautifulSoup as bs
+from bs4 import SoupStrainer
+
+#for i in range(85):
+url="http://www.gamerankings.com/browse.html?page=1"
+a=urlopen(url).read()
+b=SoupStrainer("table")
+soup=bs(a,parseOnlyThese=b)
+info=soup.find("table")
+#print str(info)
+ab=info.ul.li
+print str(ab)
+while (ab):
+      name=str(ab.a.findAll("span")[1].h3.contents[0])
+      rating=str(ab.a.span.contents[0])
+      type=str(ab.a.findAll("span")[1].span.contents[0])
+      print name , rating  , type
+      ab=ab.nextSibling.nextSibling
